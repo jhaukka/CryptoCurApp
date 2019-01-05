1d06e4e1f7b5cae7ae93689115d1a17ddebfa60f
Joonas Haukka <joonas.haukka@me.com>
2019-01-05 15:18:59 +0200

    I added sass

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./css/style.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="coin-name">Ethereum</div>
-        <br />
-        <div class="static-name">Kurssi</div>
-        <div class="coin-valuation">118,32 €</div>
-        <br />
-        <div class="static-name">Markkina-arvo</div>
-        <div class="coin-market-value">24 mrd.€</div>
+        <div class="container">
+          <div class="row">
+            <div class="col-">Kolikko</div>
+
+            <div class="col-">
+              <div className="coin-name right">Ethereum</div>
+              <br />
+              <div className="static-name right">Kurssi</div>
+              <div className="coin-valuation right">118,32 €</div>
+              <br />
+              <div className="static-name right">Markkina-arvo</div>
+              <div className="coin-market-value right">24 mrd.€</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4 col-1">1 of 2</div>
+            <div class="col-sm-4 col-2">2 of 2</div>
+            <div class="col-sm-4 col-3">1 of 3</div>
+          </div>
+        </div>
       </div>
     );
   }
